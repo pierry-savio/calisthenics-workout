0e51920b50f7d92844feec0a9288196b38dfc2f9
pierry-savio <pierrysavio.pi@gmail.com>
2026-06-25 11:59:45 -0300

    feat: monday started

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//HEADER MENU
 const menu_button = document.getElementById("menu_button");
 const mobile_menu = document.getElementById("mobile_menu");
 
@@ -5,3 +6,15 @@ menu_button.addEventListener("click", () =>{
     mobile_menu.classList.toggle("closed");
     menu_button.classList.toggle("opened");
 });
+
+//Days buttons
+const monday_btn = document.getElementById("monday_btn");
+const tuesday_btn = document.getElementById("tuesday_btn");
+const wednesday_btn = document.getElementById("wednesday_btn");
+const thursday_btn = document.getElementById("thursday_btn");
+const friday_btn = document.getElementById("friday_btn");
+const saturday_btn = document.getElementById("saturday_btn");
+
+monday_btn.addEventListener('click', () => {
+    window.location.href = "../pages/monday.html";
+})
